9aaf58096d22aba4b1ba4dfb10aece6e9150c5ca
prasad0706 <prasad070606@gmail.com>
2026-07-26 00:08:06 +0530

    Deploy

--- sub_sense_app/lib/services/api_service.dart
+++ sub_sense_app/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrl = 'https://innovahack-hsja.onrender.com';
 
   static Future<Map<String, dynamic>> register({
     required String email,
