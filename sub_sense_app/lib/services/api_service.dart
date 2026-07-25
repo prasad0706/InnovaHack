fb804ca2d428ed6b00053abd62c84be40741d2c0
Prasad Mahajan <prasad070606@gmail.com>
2026-07-26 02:59:23 +0530

    Update API base URL to production server

--- sub_sense_app/lib/services/api_service.dart
+++ sub_sense_app/lib/services/api_service.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
+  import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrl = 'https://innovahack-hsja.onrender.com';
 
   static Future<Map<String, dynamic>> register({
     required String email,
